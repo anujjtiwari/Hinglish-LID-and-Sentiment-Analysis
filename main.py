908ef6e364dd6cd969d197466746e30b8e51f9ad
suvasish114 <misuvasish114@gmail.com>
2026-08-29 09:41:21 +0530

    remove redundent object initialization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib.inference import CommertialModels
 
 # driving code
 if __name__ == "__main__":
+    # config
     model_name = "mistralai-7B-instruct-v0.3"
     train_csv_file_path = "/nlsasfs/home/aidrive/dassuv/research/sentiment_analysis/comi_lingua_dataset/train.csv"
     test_csv_file_path = "/nlsasfs/home/aidrive/dassuv/research/sentiment_analysis/comi_lingua_dataset/test.csv"
@@ -11,10 +12,7 @@ if __name__ == "__main__":
     max_tokens = 2048
     temperature = 0.0
 
-    # zero shot
-    model0 = CommertialModels(model_name, input_csv_file_path=test_csv_file_path, output_csv_file_path=output_zero_csv_file_path, max_tokens=max_tokens, temperature=temperature)
-    model0.inference_all(shot=0)
-
-    # one shot
-    model1 = CommertialModels(model_name, input_csv_file_path=test_csv_file_path, output_csv_file_path=output_one_csv_file_path, max_tokens=max_tokens, temperature=temperature)
-    model1.inference_all(shot=1)
+    # call
+    model = CommertialModels(model_name, input_csv_file_path=test_csv_file_path, output_csv_file_path=output_zero_csv_file_path, max_tokens=max_tokens, temperature=temperature)
+    model.inference_all(shot=0) # zero shot
+    model.inference_all(shot=1) # one shot
